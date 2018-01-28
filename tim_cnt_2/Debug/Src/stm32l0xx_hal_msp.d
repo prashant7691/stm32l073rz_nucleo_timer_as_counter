301b199fc20de5fe5fe1c5a50f95b5222da8e801
prashant7691 <pjn.prashant@gmail.com>
2018-01-28 12:14:36 +0530

    Count external pulses
    counting of external pulses using the TIM2 in an external mode. It counts till 10 and a reset counter function is used.
    
    Configuration for the CubeMX are:
    1. Select Timer 2
    2. Select slave mode as External Clock mode1
    3.Trigger source TI1FP1
    
    4. Go to Configuration and set timer configuration  - Counter Period ( the maximum number of counts you want)

---
+++ tim_cnt_2/Debug/Src/stm32l0xx_hal_msp.d
@@ -0,0 +1,82 @@
+Src/stm32l0xx_hal_msp.o: ../Src/stm32l0xx_hal_msp.c \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ G:/workspace/tim_cnt_2/Inc/stm32l0xx_hal_conf.h \
+ G:/workspace/tim_cnt_2/Inc/main.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/core_cm0plus.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/core_cmInstr.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/core_cmFunc.h \
+ G:/workspace/tim_cnt_2/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h \
+ G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+G:/workspace/tim_cnt_2/Inc/stm32l0xx_hal_conf.h:
+
+G:/workspace/tim_cnt_2/Inc/main.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/core_cm0plus.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/core_cmInstr.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Include/core_cmFunc.h:
+
+G:/workspace/tim_cnt_2/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h:
+
+G:/workspace/tim_cnt_2/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h:
